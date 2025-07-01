3e93d9f7417418c2013d53e2eb4754432b9b986f
Quadrifoglio Verde <quadrifoglio@protonmail.com>
2025-07-01 23:01:55 +0200

    Revert GPS, L80 in stock FW

--- src/gps/GPS.h
+++ src/gps/GPS.h
@@ -31,8 +31,7 @@ typedef enum {
     GNSS_MODEL_MTK_PA1616S,
     GNSS_MODEL_AG3335,
     GNSS_MODEL_AG3352,
-    GNSS_MODEL_LS20031,
-    GNSS_MODEL_MTK_3329
+    GNSS_MODEL_LS20031
 } GnssModel_t;
 
 typedef enum {
